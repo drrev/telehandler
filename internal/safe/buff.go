63a5fc940db320fb53361873a60f7416063f8b7f
neecapp <neecapp@protonmail.com>
2024-10-08 14:33:51 -0400

    Add threadsafe utility types

---
+++ internal/safe/buff.go
@@ -0,0 +1,75 @@
+package safe
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"sync"
+)
+
+// A Buffer is a variable-sized buffer of bytes with [Buffer.Read] and [Buffer.Write] methods.
+// The zero value MUST NEVER be used--use [NewBuffer].
+type Buffer struct {
+	cond *CondChan
+	buf  *bytes.Buffer
+	m    *sync.RWMutex
+}
+
+// Len returns the total number of bytes in the buffer.
+func (b *Buffer) Len() int {
+	b.m.RLock()
+	defer b.m.RUnlock()
+	return b.buf.Len()
+}
+
+// Write appends the contents of p to the buffer, growing the buffer as
+// needed. The return value n is the length of p; err is always nil. If the
+// buffer becomes too large, Write will panic with [ErrTooLarge].
+func (b *Buffer) Write(p []byte) (n int, err error) {
+	b.m.Lock()
+	defer b.m.Unlock()
+	defer b.cond.Broadcast()
+	return b.buf.Write(p)
+}
+
+// ReadAt implements io.ReaderAt.
+func (b *Buffer) ReadAt(p []byte, off int64) (n int, err error) {
+	b.m.RLock()
+	defer b.m.RUnlock()
+	if off > int64(b.buf.Len()) {
+		return 0, io.EOF
+	}
+
+	raw := b.buf.Bytes()
+	n = copy(p, raw[off:])
+	if len(p) > n {
+		err = io.ErrUnexpectedEOF
+	}
+	return
+}
+
+// Wait for Buffer to change.
+func (b *Buffer) Wait(ctx context.Context, seq int64) (newSeq int64) {
+	return b.cond.Wait(ctx, seq)
+}
+
+// NewBuffer creates and initializes a new [Buffer] using buf as its
+// initial contents. The new [Buffer] takes ownership of buf, and the
+// caller should not use buf after this call. NewBuffer is intended to
+// prepare a [Buffer] to read existing data. It can also be used to set
+// the initial size of the internal buffer for writing. To do that,
+// buf should have the desired capacity but a length of zero.
+//
+// In most cases, new([Buffer]) (or just declaring a [Buffer] variable) is
+// sufficient to initialize a [Buffer].
+func NewBuffer(buf []byte) *Buffer {
+	var b bytes.Buffer
+	return &Buffer{cond: NewCond(), buf: &b, m: &sync.RWMutex{}}
+}
+
+// NewBufferString creates and initializes a new [Buffer] using string s as its
+// initial contents. It is intended to prepare a buffer to read an existing
+// string.
+func NewBufferString(s string) *Buffer {
+	return NewBuffer([]byte(s))
+}
---
+++ internal/safe/condchan.go
@@ -0,0 +1,59 @@
+package safe
+
+import "context"
+
+// CondChan is a simplified channel-based implementation of
+// sync.Cond for broadcasting changes with context cancellation support.
+type CondChan struct {
+	ch chan state
+}
+
+type state struct {
+	seq    int64
+	listen chan struct{}
+}
+
+// Wait suspends the calling goroutine until a change occurs.
+//
+// It is required that seq is 0 on first wait. On subsequent Wait calls,
+// the newSeq returned by the previous wait should be used for the next Wait.
+//
+// The last seq seen is returned immediately if ctx is cancelled.
+func (c *CondChan) Wait(ctx context.Context, seq int64) (newSeq int64) {
+	st := <-c.ch
+
+	// if state changed before this Wait call, eagerly return
+	if st.seq != seq {
+		c.ch <- st
+		return st.seq
+	}
+
+	if st.listen == nil {
+		st.listen = make(chan struct{})
+	}
+	c.ch <- st
+
+	// race between ctx and change to allow cancellation
+	select {
+	case <-ctx.Done():
+		return seq
+	case <-st.listen:
+		return seq + 1
+	}
+}
+
+// Broadcast wakes all goroutines waiting on c.
+func (c *CondChan) Broadcast() {
+	st := <-c.ch
+	if st.listen != nil {
+		// closing chan broadcasts a change
+		close(st.listen)
+	}
+	c.ch <- state{st.seq + 1, nil}
+}
+
+func NewCond() *CondChan {
+	st := make(chan state, 1)
+	st <- state{seq: 0}
+	return &CondChan{st}
+}
