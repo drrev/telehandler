114d8139e2e06679d5a416bc0e7579eaef1d82d3
neecapp <neecapp@protonmail.com>
2024-10-08 17:51:02 -0400

    Update root command flags, remove unnecessary comments

--- cmd/root.go
+++ cmd/root.go
@@ -6,19 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	cert       = "ssl/server.pem"
+	key        = "ssl/server-key.pem"
+	root       = "ssl/root.pem"
+	cgroupRoot = "/sys/fs/cgroup"
+)
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "telehandler",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Telehandler is a simple service that is used to start, stop, query status, and watch the output of an arbitrary Linux process over gRPC.",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -31,13 +29,9 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.PersistentFlags().StringVarP(&cert, "cert", "c", cert, "Server cert path")
+	rootCmd.PersistentFlags().StringVarP(&key, "key", "k", key, "Server key path")
+	rootCmd.PersistentFlags().StringVarP(&root, "root", "r", root, "Root CA cert path")
+	rootCmd.PersistentFlags().StringVar(&cgroupRoot, "cgroup-root", cgroupRoot, "Path to cgroup v2 mount")
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.telehandler.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
