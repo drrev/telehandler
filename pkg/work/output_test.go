9ca97e8e3d684927f682abf6bdbcc02177f7df0e
neecapp <neecapp@protonmail.com>
2024-10-10 14:37:04 -0400

    add simple output test

---
+++ pkg/work/output_test.go
@@ -0,0 +1,74 @@
+package work
+
+import (
+	"context"
+	"math/rand/v2"
+	"testing"
+
+	"github.com/drrev/telehandler/internal/safe"
+	"github.com/drrev/telehandler/tests/utils"
+)
+
+func TestOutputReader_Read(t *testing.T) {
+	chacha := rand.NewChaCha8([32]byte{})
+
+	buff := func(sz int, closed bool) *safe.Buffer {
+		data := make([]byte, sz)
+		chacha.Read(data)
+
+		b := safe.NewBuffer(data)
+		b.Write(data)
+		if closed {
+			b.Close()
+		}
+		return b
+	}
+
+	type fields struct {
+		seq int64
+		off int64
+		max int64
+		ctx context.Context
+		out *safe.Buffer
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantN   int
+		wantErr func(error) bool
+	}{
+		{
+			name:    "closed: no data",
+			fields:  fields{ctx: context.Background(), out: buff(0, true)},
+			wantN:   0,
+			wantErr: utils.ErrorTextContains(t, "EOF"),
+		},
+		{
+			name:    "closed: backfill",
+			fields:  fields{ctx: context.Background(), max: 45, out: buff(45, true)},
+			wantN:   45,
+			wantErr: utils.NoError(t),
+		},
+	}
+
+	data := make([]byte, 4096)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &OutputReader{
+				seq: tt.fields.seq,
+				off: tt.fields.off,
+				max: tt.fields.max,
+				ctx: tt.fields.ctx,
+				out: tt.fields.out,
+			}
+			gotN, err := o.Read(data)
+			if !tt.wantErr(err) {
+				t.Errorf("OutputReader.Read() error = %v", err)
+				return
+			}
+			if gotN != tt.wantN {
+				t.Errorf("OutputReader.Read() = %v, want %v", gotN, tt.wantN)
+			}
+		})
+	}
+}
