1da2420e24e149a24693cd8ec766ddda7a8f4e82
neecapp <neecapp@protonmail.com>
2024-10-11 16:48:56 -0400

    simplify io, add test

--- pkg/cgroup2/io.go
+++ pkg/cgroup2/io.go
@@ -14,29 +14,40 @@ const (
 	ioWiopsLimit = 1000
 )
 
+// ioConstraints uses the given deviceIter to create a Constraint slice, so that
+// each block device is limited constrained.
 func ioConstraints(deviceIter iter.Seq2[string, error]) ([]Constraint, error) {
 	constraints := []Constraint{}
+	if deviceIter == nil {
+		return nil, nil
+	}
 
 	for mm, err := range deviceIter {
 		if err != nil {
 			return nil, err
 		}
 
-		v := fmt.Sprintf(
-			"%s rbps=%d wbps=%d riops=%d wiops=%d",
-			mm,
-			ioRBpsLimit,
-			ioWBpsLimit,
-			ioRiopsLimit,
-			ioWiopsLimit,
-		)
-
-		constraints = append(constraints, Constraint{"io.max", v})
+		constraints = append(constraints, Constraint{"io.max", ioMajorMinorConstraint(mm)})
 	}
 
 	return constraints, nil
 }
 
+// ioMajorMinorConstraint converts mm (major:minor) into a formatted limit
+// for Constraint.
+func ioMajorMinorConstraint(mm string) string {
+	return fmt.Sprintf(
+		"%s rbps=%d wbps=%d riops=%d wiops=%d",
+		mm,
+		ioRBpsLimit,
+		ioWBpsLimit,
+		ioRiopsLimit,
+		ioWiopsLimit,
+	)
+}
+
+// blockDevices reads all block devices from /sys/block and exposes
+// the discovered major/minor and any read errors as a [iter.Seq2].
 func blockDevices() (iter.Seq2[string, error], error) {
 	devFiles, err := filepath.Glob("/sys/block/*/dev")
 	if err != nil {
