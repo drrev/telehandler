3c1eb20777f8f2325aeffde4de6d9d26d40e2160
neecapp <neecapp@protonmail.com>
2024-10-11 09:32:51 -0400

    clean up tests

--- pkg/work/output_test.go
+++ pkg/work/output_test.go
@@ -32,27 +32,25 @@ func TestOutputReader_Read(t *testing.T) {
 	}
 	tests := []struct {
 		name    string
-		ctx     context.Context
 		fields  fields
 		wantN   int
 		wantErr func(error) bool
 	}{
 		{
 			name:    "closed: no data",
-			ctx:     context.Background(),
 			fields:  fields{out: buff(0, true)},
 			wantN:   0,
 			wantErr: utils.ErrorTextContains(t, "EOF"),
 		},
 		{
 			name:    "closed: backfill",
-			ctx:     context.Background(),
 			fields:  fields{max: 45, out: buff(45, true)},
 			wantN:   45,
-			wantErr: utils.NoError(t),
+			wantErr: utils.ErrorTextContains(t, "EOF"),
 		},
 	}
 
+	ctx := context.Background()
 	data := make([]byte, 4096)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -62,7 +60,7 @@ func TestOutputReader_Read(t *testing.T) {
 				max: tt.fields.max,
 				out: tt.fields.out,
 			}
-			gotN, err := o.Read(tt.ctx, data)
+			gotN, err := o.Read(ctx, data)
 			if !tt.wantErr(err) {
 				t.Errorf("OutputReader.Read() error = %v", err)
 				return
