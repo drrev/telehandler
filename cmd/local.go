daf30e198a7fb5fc6cc0c605a079008377c5ee81
neecapp <neecapp@protonmail.com>
2024-10-14 15:17:16 -0400

    add a simple command to run commands locally without a server

--- cmd/local.go
+++ cmd/local.go
@@ -1,9 +1,7 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"errors"
 	"io"
 	"os"
 
@@ -13,22 +11,32 @@ import (
 
 // localCmd represents the local command
 var localCmd = &cobra.Command{
-	Use: "local",
+	Use:   "local <cmd> [args...]",
+	Short: "Run the given command with cgroup and namespace enforcement.",
+	Long: `Run the given command with cgroup and namespace enforcement.
 
+The local command does not require a running Telehandler server, all commands
+are executed in the local environment.
+`,
+	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		mgr := work.NewExecutor(cgroupRoot)
 
-		job := *work.NewJob("local", args[0], args[1:])
+		job := *work.NewJob("admin", args[0], args[1:])
 		if err := mgr.Start(job); err != nil {
 			return err
 		}
 
-		r, err := mgr.Watch(job.ID)
+		r, err := mgr.OpenReader(job.ID)
 		if err != nil {
 			return err
 		}
 
-		io.Copy(os.Stdout, r)
+		_, err = io.Copy(os.Stdout, r)
+		if !errors.Is(err, io.EOF) {
+			return err
+		}
+
 		return nil
 	},
 }
